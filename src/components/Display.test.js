c5f77b9773d04b60cd36eb78566f5afb777771ac
Kyle Richardson <kyle.richardson1@gmail.com>
2019-10-04 16:56:24 -0500

    basic boilerplate for test files

--- src/components/Display.test.js
+++ src/components/Display.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom"
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Display from "./Display"
-import App from "../App"
+import App from '../App';
+
+afterEach(rtl.cleanup);
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
